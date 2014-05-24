d02d53da554aa2b78ab5e04f0a5277e6ccf05049
neozl24 <neozl24@hotmail.com>
2014-05-25 00:44:06 +0800

    follow frankobe's idea

--- median_of_two_sorted_arrays_.py
+++ median_of_two_sorted_arrays_.py
@@ -1,32 +1,40 @@
 class Solution:
     # @return a float
     def findMedianSortedArrays(self, A, B):
-        merge_list = []
-
-        index_a = 0
-        index_b = 0
-
-        while index_a < len(A) and index_b < len(B):
-            if A[index_a] <= B[index_b]:
-                merge_list.append(A[index_a])
-                index_a += 1
-            elif A[index_a] >= B[index_b]:
-                merge_list.append(B[index_b])
-                index_b += 1
-
-        if index_a == len(A):
-            merge_list.extend(B[index_b:])
-        elif index_b == len(B):
-            merge_list.extend(A[index_a:])
-
-        length = len(merge_list)
+        length = len(A) + len(B)
         if length%2 == 0:
-            return (merge_list[length/2-1]+merge_list[length/2])/2.0
+            return (self.findKthSmallest(A, B, length/2) + self.findKthSmallest(A, B, length/2 + 1))/2.0
         else:
-            return merge_list[length/2]
+            return self.findKthSmallest(A, B, length/2 + 1)
+
+    def findKthSmallest(self, A, B, k):
+        # k <= len(A) + len(B) and k > 0
+        if len(A) == 0:
+            return B[k-1]
+        elif len(B) == 0:
+            return A[k-1]
+        elif k == 1 and A[0] <= B[0]:
+            return A[0]
+        elif k == 1 and B[0] < A[0]:
+            return B[0]
+        else:
+            ia = int(k/2)
+            ib = k - ia
+            if ia > len(A):
+                ia = len(A)
+                ib = k - ia
+            elif ib > len(B):
+                ib = len(B)
+                ia = k - ib
+                
+            if A[ia - 1] <= B[ib - 1]:
+                return self.findKthSmallest(A[ia:], B, k-ia)
+            elif A[ia - 1] > B[ib - 1]:
+                return self.findKthSmallest(A, B[ib:], k-ib)
+
 
 if __name__ == '__main__':
     s = Solution()
-    print s.findMedianSortedArrays([1,2,3,6],[4,5,7,8])
+    print s.findMedianSortedArrays([2,3,4],[1])
 
     raw_input()
